b0cce836039b9a46279d7bc012a235cd2a004a4b
Stepan <empo999pr@gmail.com>
2020-05-18 20:59:38 +0300

    Animals js

---
+++ js/animals.js
@@ -0,0 +1,22 @@
+const animals = [
+  {
+    type: 'dog',
+    name: 'Jack',
+    age: 9
+  },
+  {
+    type: 'cat',
+    name: 'Mike',
+    age: 8
+  },
+  {
+    type: 'fish',
+    name: 'Nemo',
+    age: 2
+  },
+  {
+    type: 'bull',
+    name: 'Niko',
+    age: 3
+  }
+];
