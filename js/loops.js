720acdbba83e72c7c3b50b0a309d44bf51c2f2fc
Stepan <empo999pr@gmail.com>
2020-05-18 20:06:51 +0300

    Loops for loop

---
+++ js/loops.js
@@ -0,0 +1,18 @@
+let counter = 1;
+
+console.log(counter++);
+console.log(counter++);
+console.log(counter++);
+console.log(counter++);
+console.log(counter++);
+console.log(counter++);
+console.log(counter++);
+console.log(counter++);
+console.log(counter++);
+console.log(counter++);
+
+console.log('for loop ->')
+// for loop
+for (let counter = 1; counter <= 20; counter = counter + 1) {
+  document.write(counter + '<br>');
+}
